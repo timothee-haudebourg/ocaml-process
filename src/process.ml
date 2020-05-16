f70897b4c564afd907645e54475be9f11dbd204e
Timothée Haudebourg <timothee.haudebourg@irisa.fr>
2020-05-16 11:31:30 +0200

    Add a finalizer to close the processes.

--- src/process.ml
+++ src/process.ml
@@ -1,32 +1,44 @@
-type t = {
-  stdout: in_channel;
-  stderr: in_channel;
-  stdin: out_channel;
-}
-
 exception Crash
 
 type status =
   | Ok
   | Error
 
-(* type result = status * string * string *)
+type t = {
+  stdout: in_channel;
+  stderr: in_channel;
+  stdin: out_channel;
+  mutable status: status option
+}
 
-let create ?(env=Unix.environment ()) cmd =
-  let oc, ic, ec = Unix.open_process_full cmd env in
-  {
-    stdout = oc;
-    stderr = ec;
-    stdin = ic
-  }
+(* type result = status * string * string *)
 
 let get_status = function
   | Unix.WEXITED 0 -> Ok
   | _ -> Error
 
 let close t =
-  let exit_status = Unix.close_process_full (t.stdout, t.stdin, t.stderr) in
-  get_status exit_status
+  match t.status with
+  | Some status -> status
+  | None ->
+    let exit_status = Unix.close_process_full (t.stdout, t.stdin, t.stderr) in
+    let status = get_status exit_status in
+    t.status <- Some status;
+    status
+
+let finalise t =
+  ignore (close t)
+
+let create ?(env=Unix.environment ()) cmd =
+  let oc, ic, ec = Unix.open_process_full cmd env in
+  let t = {
+    stdout = oc;
+    stderr = ec;
+    stdin = ic;
+    status = None
+  } in
+  Gc.finalise finalise t;
+  t
 
 let stdout t = t.stdout
 
